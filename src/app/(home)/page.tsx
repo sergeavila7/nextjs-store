e4609c3ed725f8d34146056517258253653f9564
sergeavila7 <serge_avila7@yahoo.com>
2024-03-13 20:11:23 -0600

    add: SEO metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,13 @@
 import { MainProducts } from 'app/components';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '✨Future world',
+  description: 'World to the future world, an ecommerce from other century',
+  keywords: ['ecommere', 'future', 'world', 'technology'],
+};
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between py-24'>
