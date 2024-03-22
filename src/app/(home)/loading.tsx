57c3694c732d163da8414a581e2df8db436df90a
sergeavila7 <serge_avila7@yahoo.com>
2024-03-21 18:23:26 -0600

    fix: Loader position

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -1,9 +1,5 @@
 import { Loader } from 'app/components/shared/Loader';
 
 export default function Loading() {
-  return (
-    <div className='flex-center'>
-      <Loader />
-    </div>
-  );
+  return <Loader />;
 }
